d5441bbaf0ae1e04758e53e44a40d29186a5cbfb
ryan goreshi <ryan.goreshi@yahoo.com>
2012-09-22 18:54:26 -0700

    comit

--- projects/BusService/BusTop.api/Controllers/ordersController.cs
+++ projects/BusService/BusTop.api/Controllers/ordersController.cs
@@ -14,6 +14,7 @@ namespace BusTop.api.Controllers
 {
     public class ordersController : ApiController
     {
+        //keep this class clean
         public Guid Get()
         {
             var order = new PlaceOrder() 
@@ -23,7 +24,13 @@ namespace BusTop.api.Controllers
                 productId=Guid.NewGuid()
 
             };
-            order.SetHeader("access_token", HttpContext.Current.Request.Params["access_token"]);
+
+            
+            //order.SetHeader("access_token", HttpContext.Current.Request.Params["access_token"]);
+            //URL : http://localhost:1350/api/orders?access_token=busstop
+            //sending messages with authentication each time , instead we can send multiple messages in one message in form of mutator
+            //then move it to other partiotion 
+
             WebApiApplication.Bus.Send( order); //then use config file   add message to endpoint
             return order.orderId;
         }
---
+++ projects/BusService/BusTop.api/authentication/AccessTokenMutator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NServiceBus.MessageMutator;
+using NServiceBus.Config;
+using NServiceBus;
+
+namespace BusTop.api.authentication
+{
+    public class AccessTokenMutator:IMutateOutgoingTransportMessages , INeedInitialization
+    {
+        void IMutateOutgoingTransportMessages.MutateOutgoing(object[] messages, NServiceBus.Unicast.Transport.TransportMessage transportMessage)
+        {
+            //gets it from Url or manually
+            transportMessage.Headers["access_token"] = "busstop";//HttpContext.Current.Request.Params["access_token"];
+        }
+
+        void INeedInitialization.Init()
+        {
+            Configure.Instance.Configurer.ConfigureComponent<AccessTokenMutator>
+                (
+                    DependencyLifecycle.InstancePerCall
+                );
+        }
+    }
+}
