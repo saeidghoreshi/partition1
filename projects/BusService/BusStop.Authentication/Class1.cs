fe99c9454d992f5364e545091d5a916203ea5b10
ryan goreshi <ryan.goreshi@yahoo.com>
2012-09-22 17:05:10 -0700

    start working on busstop authentication

---
+++ projects/BusService/BusStop.Authentication/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusStop.Authentication
+{
+    public class Class1
+    {
+    }
+}
--- projects/BusService/BusStop.Backend/PlaceOrderHandler.cs
+++ projects/BusService/BusStop.Backend/PlaceOrderHandler.cs
@@ -5,18 +5,40 @@ using System.Text;
 using NServiceBus;
 using BusTop.Contract;
 
+using Raven.Client.Document;
+using Raven.Client;
+
 namespace BusStop.Backend
 {
     public class PlaceOrderHandler:IHandleMessages<PlaceOrder>
     {
+        public IDocumentSession Session { get; set; }
+        //the same session asking for unit of work
+
         public void Handle(PlaceOrder message)
         {
-            throw new Exception("Database is Down");
+            //Fault tolerance
+            //throw new Exception("Database is Down");
+
+
+            Session.Store(new Order()
+            {
+                OrderId = message.orderId
+            });
+
+
+            //throw new Exception("Some Random Failure");
+            //Note that because of exception, transaction wont be completed and be rollbacked
+
             //after tries defined in config file then msg goes to error box
             //then can use install package nservicebus.tools   on web.api project to return error messages to main queues for processing 
-            //go to packages\NServiceBus.Tools.3.2.8\tools
-            Console.WriteLine("Order Received : " +message.orderId);
+            //go to mpackages\NServiceBus.Tools.3.2.8\tools
+            Console.WriteLine("Order Received : " + message.orderId);
         }
+    }
+    public class Order 
+    {
+        public Guid OrderId { get; set; }
     }
 }
     
