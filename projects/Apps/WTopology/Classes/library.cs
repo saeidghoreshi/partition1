52c64c629e823f14a76deb1962b50f71ec366805
ryan goreshi <ryan.goreshi@yahoo.com>
2012-11-25 23:01:25 -0800

    commit addresse sproject version 1 done

---
+++ projects/Apps/WTopology/Classes/library.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WTopology.Classes
+{
+    public class treeNode
+    {
+        public Nullable<int> ID;
+        public Nullable<int> parentID;
+        public string title;
+        public string colorCode;
+        public string description;
+
+        public List<treeNode> children;
+    }
+    public class flatNode
+    {
+        public Nullable<int> ID;
+        public Nullable<int> parentID;
+        public string title;
+        public string colorCode;
+        public string description;
+    }
+    public static class library
+    {
+        public static List<treeNode> categorizeTree(DataTable dt)
+        {
+            List<treeNode> tree = new List<treeNode> { };
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                if (string.IsNullOrEmpty(dt.Rows[i]["parentID"].ToString()))
+                {
+                    var node = new treeNode()
+                    {
+                        ID = dt.Rows[i]["ID"] as Nullable<int>,
+                        parentID = dt.Rows[i]["parentID"] as Nullable<int>,
+                        title = dt.Rows[i]["title"] as string,
+                        colorCode = dt.Rows[i]["colorCode"] as string,
+                        description = dt.Rows[i]["description"] as string,
+
+                        children = new List<treeNode>() { }
+                    };
+                    tree.Add(node);
+                }
+            }
+
+
+            foreach (var item in tree)
+                library.categorizeTreeRec(item, dt);
+
+            return tree;
+        } //BFS
+
+        public static void categorizeTreeRec(treeNode node, DataTable dt)
+        {
+            for (int j = 0; j < dt.Rows.Count; j++)
+            {
+                if (dt.Rows[j]["parentID"] as Nullable<int> == node.ID)
+                {
+                    var _node = new treeNode()
+                    {
+                        ID = dt.Rows[j]["ID"] as Nullable<int>,
+                        parentID = dt.Rows[j]["parentID"] as Nullable<int>,
+                        title = dt.Rows[j]["title"].ToString(),
+                        colorCode = dt.Rows[j]["colorCode"] as string,
+                        description = dt.Rows[j]["description"].ToString(),
+
+                        children = new List<treeNode>()
+                    };
+                    node.children.Add(_node);
+                }
+            }
+            for (int j = 0; j < node.children.Count; j++)
+                categorizeTreeRec(node.children[j], dt);
+        }
+
+        public static List<flatNode> categorizeFlat(DataTable dt)   //DFS
+        {
+            List<flatNode> Flat = new List<flatNode> { };
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                if (string.IsNullOrEmpty(dt.Rows[i]["parentID"].ToString()))
+                {
+                    var node = new flatNode()
+                    {
+                        ID = dt.Rows[i]["ID"] as Nullable<int>,
+                        parentID = dt.Rows[i]["parentID"] as Nullable<int>,
+                        title = dt.Rows[i]["title"] as string,
+                        colorCode = dt.Rows[i]["colorCode"] as string,
+                        description = dt.Rows[i]["description"] as string
+                    };
+                    Flat.Add(node);
+                    library.categorizeFlatRec(Flat, node, dt);
+                }
+            }
+
+            return Flat;
+        }
+
+        public static void categorizeFlatRec(List<flatNode> Flat, flatNode node, DataTable dt)
+        {
+            for (int j = 0; j < dt.Rows.Count; j++)
+            {
+                if (dt.Rows[j]["parentID"] as Nullable<int> == node.ID)
+                {
+                    var _node = new flatNode()
+                    {
+                        ID = dt.Rows[j]["ID"] as Nullable<int>,
+                        parentID = dt.Rows[j]["parentID"] as Nullable<int>,
+                        title = dt.Rows[j]["title"] as string,
+                        colorCode = dt.Rows[j]["colorCode"] as string,
+                        description = dt.Rows[j]["description"] as string
+                    };
+                    Flat.Add(_node);
+                    library.categorizeFlatRec(Flat, _node, dt);
+                }
+            }
+        }
+    }
+}
---
+++ projects/Apps/WTopology/obj/Release/Package/PackageTmp/Resources/javascript/home-main.js
@@ -0,0 +1,106 @@
+﻿(function ($) {
+
+    var Person = Class.create({
+        initialize: function (name) {
+            this.name = name;
+        },
+        say: function (message) {
+            return this.name + ': ' + message;
+        }
+    });
+
+    var Pirate = Class.create(Person, {
+        // redefine the speak method
+        say: function ($super, message) {
+            return $super(message) + ', yarr!';
+        }
+    });
+
+    //var john = new Pirate('Long John');
+    //var result = john.say('ahoy matey');
+
+    //console.log(result);
+
+
+} (Prototype));
+
+
+
+
+
+
+(function ($$) {
+    
+    
+    $$(document).ready(function () {
+        $$('.item_child').ready(function () {
+            $$('.item_child').animate({height: 'toggle'},0,function(){});
+        });//itemChild start
+
+        $$(".item_parent").click(function()
+        {
+            var parentID=$$(this).attr("id");
+            $$("."+parentID).animate({height: 'toggle'},1,function(){});
+        });
+
+        //main-search  effects
+        $$(".main-search").focus(
+            function()
+            {
+                if($$(".main-search:eq(0)").val()=='search')
+                    $$(this).val("");
+            }
+        );
+        $$(".main-search").blur(
+            function()
+            {
+                if($$(".main-search:eq(0)").val()=='')
+                    $$(this).val("search");
+            }
+        );
+        $$(".main-search").keyup(
+            function()
+            {
+                $$.ajax(
+                {
+                    url:"/home/jSON_getSearchResult",
+                    type:"post",
+                    data:{criteria:$$(".main-search:eq(0)").val()}
+                }).done(function(data)
+                {
+                    var result="";
+                    for(var index=0;index<data.length;index++)
+                    {
+                        if (data[index].parentID == null)
+                        {
+                            result+='<div class="item_parent" id="parent-'+data[index].ID+'">'+data[index].title+'</div>'
+                        }
+                        else
+                        { 
+                            result+='<div class="parent-'+data[index].parentID+' item_child" >';
+                                result+='<table>';
+                                result+='<tr>';
+                                result+='<td><input type="checkbox" name="main_resources_cb" id="main_resources_cb_'+data[index].ID+'" value="'+data[index].ID+'" /></td>';
+                                result+='<td><label for="main_resources_cb_'+data[index].ID+'" style="background:'+data[index].colorCode+'">'+data[index].title+'</label></td>';
+                                result+='</tr>';
+                                result+='</table>';
+                            result+='</div>';
+                        }
+                    }
+                    $$('#mainResult').html(result);
+                });
+            }
+        );
+
+        $$('#main-collapseAll').change(function()
+        {
+            $$('.item_child').each(function(){   $$(this).slideUp();});
+        });
+        $$('#main-expandAll').change(function()
+        {
+            $$('.item_child').each(function(){   $$(this).slideDown();});
+        });
+        
+
+    });
+} (jQuery));
