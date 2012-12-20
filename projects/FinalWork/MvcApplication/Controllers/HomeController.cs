c5ec0a14486476592c2c04d21ca5fd715b32d246
ryan goreshi <ryan.goreshi@yahoo.com>
2012-12-20 11:39:06 -0800

    commit

---
+++ projects/FinalWork/MvcApplication/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        [ActionName("Index")]
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+    }
+}
