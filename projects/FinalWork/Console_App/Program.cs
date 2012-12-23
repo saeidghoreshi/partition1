c7dba4feb77819470128c4d4aa6a3b6c9dab68b2
ryan goreshi <ryan.goreshi@yahoo.com>
2012-12-23 15:13:31 -0800

    changes

--- projects/FinalWork/Console_App/Program.cs
+++ projects/FinalWork/Console_App/Program.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+
 namespace Console_App
 {
     class Program
     {
         static void Main(string[] args)
         {
+            
+            Console.ReadLine();
         }
+        
     }
+  
 }
