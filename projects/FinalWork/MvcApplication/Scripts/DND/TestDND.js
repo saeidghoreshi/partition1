68e22eaeb26ce5908cd6ec34e696cf17bfcfc252
ryan goreshi <ryan.goreshi@yahoo.com>
2012-12-23 13:49:19 -0800

    dnd start

--- projects/FinalWork/MvcApplication/Scripts/DND/TestDND.js
+++ projects/FinalWork/MvcApplication/Scripts/DND/TestDND.js
@@ -15,7 +15,15 @@
                 var me = this;
 
                     //Draggables
-                    $('#draggables').children().draggable();
+                    $('#draggables').children().draggable
+                    (
+                        {   
+                            scroll: true ,
+                            scrollSensitivity: 100,
+                            scrollSpeed: 100
+                        }
+                    ).disableSelection();
+
 
                     //Sortable
                     $('#sortablesY').sortable
@@ -24,20 +32,88 @@
 						    axis:"y",
                             placeholder: "placeholder"
 						}
-					);
+					).disableSelection();
                     $('#sortablesX').sortable
 					(
 						{
 						    axis:"x",
-                            placeholder: "placeholder horizantal"
+                            placeholder: "placeholder SIDEBYSIDE"
 						}
-					);
+					).disableSelection();
                     
+                     $( "#sortableGrid" ).sortable
+                    (
+                        {
+                            placeholder: "placeholder SIDEBYSIDE"
+                        }
+                    ).disableSelection();
 
 
 
 
 
+
+
+       
+                    $( "#sortable2, #sortable3" ).sortable({
+
+                        revert: 'invalid',
+                        connectWith: ".connectedSortable"//means can drag drop on .connectedSortable objects not neccessary reverse if itself dont have it
+                    }).disableSelection();
+
+                    $( "#sortable1").sortable({
+
+                        revert: 'invalid',
+                        connectWith: ".connectedSortable",
+                        //items: "li:not(.ui-state-disabled)",   //more strict than "cancel"
+                        cancel: ".ui-state-disabled"
+
+                    })
+                    .disableSelection()
+                    .droppable(
+                    {
+                            accept: "#sortable2 li",
+                            drop: function (event, ui) {
+                                $item = ui.draggable;
+                                
+                                //get ther clone and change its properties
+//                                $item.clone()
+//                                .css({position:""})
+//                                .text("Newly Added")
+//                                .css({display:'none'})
+//                                .appendTo("#sortable3").fadeIn(1000);
+
+                                //OR 
+                                //strip the ui.draggable and build new item 
+                                $drop_pos=$('#sortable3')
+                                .find("li:eq(0)");
+
+                                $( "<li></li>" )
+                                .text( ui.draggable.text())
+                                .addClass('ui-state-active')
+                                .css({display:"none"})
+                                .insertAfter( $drop_pos)
+                                .fadeIn(function()
+                                {
+                                    $(this).animate(
+                                    {
+                                        "font-weight":'bold'
+                                    });
+                                });
+
+
+                                //
+                                $item.clone()
+                                .css({position:""})
+                                .text('*****')
+                                .css({display:'none'})
+                                .appendTo("#sortable2").fadeIn(2000);
+                                
+                            }
+                    });
+
+
+                   
 
 
 
