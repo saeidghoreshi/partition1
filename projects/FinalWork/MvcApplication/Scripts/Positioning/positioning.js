f6a6e32df2dd6a066c435964d8d7a825f89b622a
ryan goreshi <ryan.goreshi@yahoo.com>
2012-12-23 18:15:08 -0800

    start on positioning

---
+++ projects/FinalWork/MvcApplication/Scripts/Positioning/positioning.js
@@ -0,0 +1,29 @@
+﻿var positioning;
+(function ($$) {
+    (function ($) {
+
+        TestDND= Class.create({
+
+            initialize: function () {
+
+                var me=this;
+                me.buildUI();
+            },
+
+            //on ViewPort3
+            buildUI: function () {
+                var me = this;
+
+                    
+
+            }
+
+            
+        });
+        
+      
+    } (jQuery));
+} (Prototype));
+
+
+var ins1 = new positioning();
---
+++ projects/FinalWork/MvcApplication/Scripts/startPositioning.js
@@ -0,0 +1,7 @@
+﻿(function ($$) {
+    (function ($) {
+
+        JSCSSLOADER.loadAll(['../scripts/positioning/positioning.js'], ['../scripts/positioning/positioning.css'], null);
+
+    } (jQuery));
+} (Prototype));
