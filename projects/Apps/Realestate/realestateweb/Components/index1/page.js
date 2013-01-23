013b73c281a7f4c5de006f0cf736f754712bec2c
ryan goreshi <ryan.goreshi@yahoo.com>
2013-01-22 22:15:28 -0800

    some changes in realestate projects

--- projects/Apps/Realestate/realestateweb/Components/index1/page.js
+++ projects/Apps/Realestate/realestateweb/Components/index1/page.js
@@ -1,3 +1,38 @@
-﻿var mygmap = new gmap({ parentID: "center-area", imageRoot: "/components/index1/img" });
+﻿(function ($$) {
+    (function ($) {
 
-jQuery('#listings > ul').sortable({axis:"y"});
+        var mygmap = new gmap({ parentID: "center-area", imageRoot: "/components/index1/img" });
+
+        jQuery('#listings > ul').sortable({ axis: "y" });
+
+        jQuery('#listings ul li').click(function () {
+            var data = jQuery(this).attr('data');
+            data = jQuery.parseJSON(data);
+
+            $('.listings_selected').removeClass('listings_selected');
+            $(this).toggleClass('listings_selected');
+
+            jQuery('#center-area').gmap3({
+                map: {
+                    options: {
+                        mapTypeId: google.maps.MapTypeId.HYBRID,
+                        center:
+                {
+                    latLng: [data.lat, data.lng]
+                },
+                        zoom: 16
+                    }
+                },
+                marker: {
+                    values: [
+                       {
+                           latLng: [data.lat, data.lng], data: "", options: { icon: "/components/index1/img/gmap_pin_grey.png" }
+                       }
+                    ]
+                }//Markers
+
+            });
+        });
+
+    } (jQuery));
+} (Prototype));
--- projects/Apps/Realestate/realestateweb/Controllers/HomeController.cs
+++ projects/Apps/Realestate/realestateweb/Controllers/HomeController.cs
@@ -29,15 +29,6 @@ namespace realestateweb.Controllers
             return PartialView("index1/listings");
         }
 
-        public object getPureView(string viewName)
-        {
-            var sw = new System.IO.StringWriter();
-            var viewResult = ViewEngines.Engines.FindPartialView(ControllerContext, viewName);
-            var viewContext = new ViewContext(ControllerContext, viewResult.View, ViewData, TempData, sw);
-            viewResult.View.Render(viewContext, sw);
-            viewResult.ViewEngine.ReleaseView(ControllerContext, viewResult.View);
-
-            return sw.GetStringBuilder().ToString();
-        }
+        
     }
 }
